11abddbd8d30186b54ee4a4c7b1eb5c51ba658c6
lucidlactose <jesusvillarruel98@gmail.com>
2021-05-11 11:10:58 -0700

    the project stuff

---
+++ server/client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <h1>
+                utasgduyasdd
+            </h1>
+            Collect feedback frmo users
+        </div>
+    )
+}
+
+export default Landing; 
